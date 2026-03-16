00a9397e40b3a10e8acf83ec636bfe9c7a9e21ce
Ascendix <official@ascendix.in>
2026-03-16 20:17:32 +0530

    Update check.py

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 # simple status variable
-status_code = 1
+status_code = 0
 @app.get("/")
 def root():
     return {"code": status_code}
